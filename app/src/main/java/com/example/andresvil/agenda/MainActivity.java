9cab5c860c1203b6d3b70139992ebba608602a23
Andres Villarreal <andresvil@gmail.com>
2016-03-22 18:17:49 -0600

    initial commit add 2 buttons, calendarView, load and set typeface

---
+++ app/src/main/java/com/example/andresvil/agenda/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.andresvil.agenda;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find buttons and their textviews
+        Button mesButton = (Button) findViewById(R.id.buttonMes);
+        Button consultasButton = (Button) findViewById(R.id.buttonConsultas);
+        TextView mesText = (TextView) findViewById(R.id.buttonMes);
+        TextView consultasText = (TextView) findViewById(R.id.buttonConsultas);
+
+        // Load font
+        AssetManager am = this.getApplicationContext().getAssets();
+        Typeface myFont = Typeface.createFromAsset(am, String.format(Locale.US, "fonts/%s", "AvenirLTStd-Roman_0.otf"));
+
+        // Set TextViews to myFont
+        mesText.setTypeface(myFont);
+        consultasText.setTypeface(myFont);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
